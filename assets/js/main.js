d7a89d7c0176c60c1b1fb15054d48cf28d918cb5
Dana Franco <turquoise.ilg@hotmail.com>
2017-04-28 12:12:03 -0500

    validating

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,20 @@
-dni = document.getElementById("dni").value;
+//dni = document.getElementById("dni").value;
 
-if( [0-9].test(dni)) ) {
-  return false;
+//if( 	/^\d{9}$/.test(dni) ) {
+//  return false;
+//}
+
+
+function validateForm(){
+	/* Escribe tú código aquí */
+
+  var nombre= document.getElementById("name");
+
+  if(nombre==""){
+    alert("campo obligatorio");
+  }
+  document.getElementById("formu").onsubmit = function(e){
+    e.preventDefault();
+
+
 }
